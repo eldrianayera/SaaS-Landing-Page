cc2f7534886bb440acec6ae294810df2d8db31ff
Eldrian <eldrianayera710@gmail.com>
2025-07-30 20:30:10 +0800

    fix navbar error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,62 +23,68 @@ export const NavBar = () => {
   }, []);
 
   return (
-    <nav
-      className={cn(
-        "fixed z-50 transition-all duration-300 w-full",
-        isScrolled ? "py-2  backdrop-blur-md shadow-xs" : "py-5"
-      )}
-    >
-      <div className=" flex items-center justify-between max-md:justify-evenly container max-w-7xl lg:px-20 md:px-15">
-        <div className="">
-          <a href="#hero" className="text-4xl font-semibold">
-            Eldrian<span className="text-primary font-extrabold">AI</span>
-          </a>
-        </div>
-        {/* Desktop Navbar */}
-        <div className="flex justify-center lg:gap-9 max-md:hidden ">
-          {NavItem.map((item, key) => {
-            return (
-              <a key={key} className="rounded-full py px-2" href={item.href}>
-                {item.name}
-              </a>
-            );
-          })}
-        </div>
+    <>
+      <nav
+        className={cn(
+          "fixed z-40 transition-all duration-300 w-full",
+          isScrolled ? "py-2  backdrop-blur-md shadow-xs" : "py-5"
+        )}
+      >
+        <div className=" flex items-center justify-between max-md:justify-evenly container max-w-7xl lg:px-20 md:px-15">
+          <div className="">
+            <a href="#hero" className="text-4xl font-semibold">
+              Eldrian<span className="text-primary font-extrabold">AI</span>
+            </a>
+          </div>
+          {/* Desktop Navbar */}
+          <div className="flex justify-center lg:gap-9 max-md:hidden ">
+            {NavItem.map((item, key) => {
+              return (
+                <a key={key} className="rounded-full py px-2" href={item.href}>
+                  {item.name}
+                </a>
+              );
+            })}
+          </div>
 
-        <div className="flex items-center gap-3">
-          <button className="primary-button">Get Started</button>
-          <button
-            className="md:hidden z-50"
-            onClick={() => setIsMenuOpen((prev) => !prev)}
-          >
-            {isMenuOpen ? <X /> : <Menu />}
-          </button>
-          <ThemeToggle />
+          <div className="flex items-center gap-3">
+            <button className="primary-button">Get Started</button>
+            <button className="md:hidden" onClick={() => setIsMenuOpen(true)}>
+              <Menu />
+            </button>
+            <ThemeToggle />
+          </div>
         </div>
 
         {/* Mobile Navbar */}
-        <div
-          className={cn(
-            "fixed inset-0 flex flex-col items-center justify-center bg-background/95 backdrop-blur-md z-40",
-            isMenuOpen
-              ? "opacity-100 pointer-events-auto"
-              : "opacity-0 pointer-events-none"
-          )}
+      </nav>
+      <div
+        className={cn(
+          "fixed inset-0 flex flex-col items-center justify-center bg-background/95",
+          "backdrop-blur-md z-50 transition-all duration-300",
+          isMenuOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        )}
+      >
+        <button
+          className="md:hidden absolute top-6 right-6"
+          onClick={() => setIsMenuOpen(false)}
         >
-          <div className="flex flex-col space-y-8 text-xl">
-            {NavItem.map((item, key) => (
-              <a
-                onClick={() => setIsMenuOpen((prev) => !prev)}
-                key={key}
-                href={item.href}
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
+          <X />
+        </button>
+        <div className="flex flex-col space-y-8 text-xl">
+          {NavItem.map((item, key) => (
+            <a
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+              key={key}
+              href={item.href}
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
       </div>
-    </nav>
+    </>
   );
 };
